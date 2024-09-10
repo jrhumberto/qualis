0c0bb329d9118aff3d3730d5acb341d9521598b4
Humberto BM Junior <64866505+jrhumberto@users.noreply.github.com>
2024-09-10 16:13:11 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from typing import Union
+from fastapi import FastAPI
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: item, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/issn/{item_id}")
+def read_item(item_id: item, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/areas/{item_id}")
+def read_item(item_id: item, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
